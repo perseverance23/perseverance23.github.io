265d360526d00372126499c4e93dec38a8e8bcc4
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-04-06 14:05:44 +0200

    added changes to my file

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.element', {
+  strings: ["an HTML expert", "a CSS specialist", "a soon to be fully fledged web developer", "hungry", "going to get snacks now", "gonna go eat for a bit", "going to be back soon, maybe", "gonna take a break and put all this on loop"],
+  typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true
+});
